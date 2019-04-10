552d89095fb40957f74c7d757b5dfd293b6145f2
CharliePRamsey <parkerrmsy@gmail.com>
2019-04-10 15:16:13 -0400

    fixed Joey's code

--- src/components/Uploader/Uploader.js
+++ src/components/Uploader/Uploader.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 
 class Uploader extends Component {
@@ -7,7 +8,15 @@ class Uploader extends Component {
   render () {
     return (
 
-      <InputGroup.File />
+      <div class="input-group mb-3">
+        <div class="input-group-prepend">
+          <span class="input-group-text">Upload</span>
+        </div>
+        <div class="custom-file">
+          <input type="file" class="custom-file-input" id="inputGroupFile01"></input>
+          <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+        </div>
+    </div>
 
     )
   }
